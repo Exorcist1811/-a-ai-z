9e608cbbbdb86dbd11325a1760ab6df3c0d72b2a
Exorcist1811 <95427691+Exorcist1811@users.noreply.github.com>
2021-12-02 21:49:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "?",
+    introTitle: 'Ủa gòi bạn là ai z ?',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Anh ghét em đúng khum >:D',
+    desc: 'Anh ghét em thật rồi :< ',
+    btnYes: 'Đúng rồi, ghét em thật sự',
+    btnNo: 'iu em muốn chớt sao mà ghét',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao lại ghét em ?',
+    btnReply: 'Gửi cho Xỏ <3',
+    reply: 'Cô có là cái thá gì đâu mà bắt tôi phải yêu cô',
+    mess: 'À, ra là z',
+    messDesc: 'Lần sau đừng có chơi với em nữa',
+    btnAccept: 'Okiiiii lun <3'
 }
