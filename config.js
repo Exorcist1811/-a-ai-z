dfe96a38a8be74e5ae188159c2c17ae6fbdfc8a4
Exorcist1811 <95427691+Exorcist1811@users.noreply.github.com>
2021-12-02 22:13:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "???",
+    introTitle: 'Bạn có bị thiểu năng không ?',
+    introDesc: `Vừa thiểu năng vừa xấu tính`,
+    btnIntro: 'Có con cak',
+    title: 'Tôi hỏi lại, bạn có bị thiểu năng không ?',
+    desc: 'Nhớ nhìn lại bản thân nhé Tlinh với Huyền',
+    btnYes: 'Đúng gòi mình bị thiểu lăng á ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại bị thiểu năng ?',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Đúng gòi mình là siêu cấp vũ trụ thiểu năng',
+    mess: 'T biết mà lũ súc vật chúng mài',
+    messDesc: 'Mai sang nhà t chơi nhe',
+    btnAccept: 'Okiiiii lun <3'
 }
